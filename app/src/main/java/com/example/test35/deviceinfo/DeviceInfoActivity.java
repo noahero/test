73b3a54169125d8b2dfc4a0431d26d1d7fb999de
shijc <2624515457@qq.com>
2019-10-09 17:29:33 +0800

    获取设备信息

--- app/src/main/java/com/example/test35/deviceinfo/DeviceInfoActivity.java
+++ app/src/main/java/com/example/test35/deviceinfo/DeviceInfoActivity.java
@@ -1,10 +1,15 @@
 package com.example.test35.deviceinfo;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 
@@ -13,6 +18,8 @@ import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.runtime.Permission;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -61,6 +68,11 @@ public class DeviceInfoActivity extends AppCompatActivity {
                 })
                 .start();
     }
+
+    /**
+     * 获取mac地址，亲测有效
+     * @return
+     */
     public  String getNewMac() {
         try {
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
@@ -88,11 +100,19 @@ public class DeviceInfoActivity extends AppCompatActivity {
         }
         return null;
     }
+
+    /**
+     * 由蓝牙那边获取设备mac地址，但是这个6.0以后就会变成02:00:00:00:00:00，已经不能正常使用了
+     * @return
+     */
     public  String getNewMac1(){
         BluetoothAdapter myDevice = BluetoothAdapter.getDefaultAdapter();
         String deviceName = myDevice.getAddress();
         return deviceName;
     }
+
+
+
     //获取本地IP
     public  String getLocalIpAddress() {
         try {
@@ -113,10 +133,73 @@ public class DeviceInfoActivity extends AppCompatActivity {
 
         return null;
     }
+    /**
+     * 获取当前ip地址
+     *
+     * @param context
+     * @return
+     */
+    public  String getLocalIpAddress(Context context) {
+        try {
 
+            WifiManager wifiManager = (WifiManager) context
+                    .getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int i = wifiInfo.getIpAddress();
+            return int2ip(i);
+        } catch (Exception ex) {
+            return " 获取IP出错鸟!!!!请保证是WIFI,或者请重新打开网络!\n" + ex.getMessage();
+        }
+        // return null;
+    }
+    /**
+     * 将ip的整数形式转换成ip形式
+     *
+     * @param ipInt
+     * @return
+     */
+    public  String int2ip(int ipInt) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ipInt & 0xFF).append(".");
+        sb.append((ipInt >> 8) & 0xFF).append(".");
+        sb.append((ipInt >> 16) & 0xFF).append(".");
+        sb.append((ipInt >> 24) & 0xFF);
+        return sb.toString();
+    }
+
+    //GPRS连接下的ip
+    public String getGPRSIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("WifiPreference", ex.toString());
+        }
+        return null;
+    }
+    /**获取手机的IMEI号码*/
+    public  String getPhoneIMEI() {
+        TelephonyManager mTm = (TelephonyManager) DeviceInfoActivity.this.getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = mTm.getDeviceId();
+        String imsi = mTm.getSubscriberId();
+        String mtype = android.os.Build.MODEL; // 手机型号
+        String numer = mTm.getLine1Number(); // 手机号码，有的可得，有的不可得
+        return imei;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void getMac1(View view) {
-        Log.e("tag",getNewMac());
-        Log.e("tag",getNewMac1());
-        Log.e("tag",getLocalIpAddress());
+        Log.e("tag1",getNewMac());
+        Log.e("tag2",getNewMac1());
+        Log.e("tag4",getLocalIpAddress());
+        Log.e("tag5",getLocalIpAddress(this));
+        Log.e("tag6",getGPRSIpAddress());
+        Log.e("tag7",getPhoneIMEI());
     }
 }
